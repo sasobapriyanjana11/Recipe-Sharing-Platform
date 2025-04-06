ff49daa9f3b720d57878b0e900f1e39869c10c8e
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-04-06 15:06:49 +0530

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+      <>
+          <h1 className="font-poppins text-4xl font-semibold">Flavor Exchange</h1>
+          <p className="font-poppins text-base">Your favorite food-sharing platform!</p>
+
+
+      </>
+  )
+}
+
+export default App
