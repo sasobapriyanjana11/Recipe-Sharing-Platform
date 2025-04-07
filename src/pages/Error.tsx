e9a6ca415fd622b73b1a6f55c65ac5bfe23d7a3c
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-04-07 11:46:32 +0530

    implement error page

---
+++ src/pages/Error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+    return (
+        <div>
+            <h1>Welcome to the Dashboard</h1>
+            <p>This is your personalized dashboard.</p>
+        </div>
+    )
+}
+export default Error;
